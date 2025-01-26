08dfdf7ed8712796bb15ad2288f69f3f4a1ad2c6
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-01-26 03:22:40 +0000

    Jan 26, 2025, 3:22 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,6 +1,12 @@
 # print("Hello from lesson 3")
-# 
-# 
+# Adam: 60
+# Brandon: 58
+# Charlie: 98
+# Daniel: 79
+# step 1: add all scores together
+# step 2: divide scores by number of students
+# step 3: print answer
+# # 
 # 
 # 
 # 
