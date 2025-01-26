7f4b4fdc348ac8a2f267031d15bfc7aaf3558452
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-01-26 07:34:31 +0000

    Jan 26, 2025, 7:34 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -10,3 +10,22 @@ age = 12
 print(age)
 x = 10
 print(x)
+variable1 = 3
+variable2 = 5
+ans = variable1 + variable2
+print(ans)
+product = variable1 * variable2
+division = variable1 / variable2
+differences = variable1 - variable2
+ans = variable1 * variable2
+print(division)
+print (product)
+print(differences)
+a = 3
+b = 4
+c =5 
+d =10
+e =2
+calculate = (a + b)*c-(d/e)
+ans = calculate
+print(ans)
