8f77963984b866c03797729a12acb014b3b8bf0f
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-02-16 03:31:55 +0000

    Feb 16, 2025, 3:31 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,29 @@
-name = input("name?")
-age = input("age?")
-messsage = input("personal message?")
-print("Happy " + age + "th birthday " + name + "!" + " " + messsage)
+# name = input("name?")
+# age = input("age?")
+# messsage = input("personal message?")
+# print("Happy " + age + "th birthday " + name + "!" + " " + messsage)
+def create_invitation():
+    # Get user input
+    name = input("Enter the birthday person's name: ")
+    age = input("Enter the age they are turning: ")
+    message = input("Enter a personal message: ")
+    
+    # Generate invitation
+    invitation = f"""
+    🎉🎂 You're Invited! 🎂🎉
+    
+    Dear {name},
+    
+    Happy {age}th Birthday! 🥳
+    
+    {message}
+    
+    Looking forward to celebrating with you!
+    """
+    
+    # Print invitation
+    print("\nYour personalized birthday invitation:\n")
+    print(invitation)
+
+# Run the function
+create_invitation()
