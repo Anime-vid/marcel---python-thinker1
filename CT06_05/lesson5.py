4638fce599a7de4f9943a8e6465cdfdb5dab271e
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-02-16 03:29:52 +0000

    Feb 16, 2025, 3:29 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,4 @@
 name = input("name?")
 age = input("age?")
 messsage = input("personal message")
-print("Happy ") + age + ("th birthday") + name +("") + messsage
+print("Happy ") + age + ("th birthday ") + name +("!") + messsage
