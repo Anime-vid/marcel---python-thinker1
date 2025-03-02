8494e5349538484d4cbe68797acb4f1129a40ada
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-02 03:33:40 +0000

    Mar 2, 2025, 3:33 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,6 +1,8 @@
 # for i in range(1,11,1):
 #     print(i)
-for i in range(2,20,2):
-    print(i)
-for b in range(10,0,-1):
-    print(b)
+# for i in range(2,20,2):
+#     print(i)
+# for b in range(10,0,-1):
+#     print(b)
+for i in range(1,10):
+    print("burger")
