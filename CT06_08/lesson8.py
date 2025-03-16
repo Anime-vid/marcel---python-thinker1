7da7797f1d3257700cbd0473a25ea3f701a717af
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 04:31:03 +0000

    Mar 16, 2025, 4:31 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -20,4 +20,4 @@ for i in range(questions):
     num1 = (random.randint(1,50))
     num2 = (random.randint(1,50))
     ans = input("what is " + str(num1) + "+" + str(num2))
-    print(num1 + num2 == int(ans))
+    print(num1 * num2 == int(ans))
