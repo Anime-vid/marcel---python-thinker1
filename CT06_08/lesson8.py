1c6d1c7ec6a202a65a707f6cb9383825d7d16ed5
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 03:49:13 +0000

    Mar 16, 2025, 3:49 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,8 +1,8 @@
-import time
-count = int(input("Countdown?"))
-for i in range(count,0,-1):
-    print(i)
-    time.sleep(1)
-print("liftoff")
-import random
-random
+# import time
+# count = int(input("Countdown?"))
+# for i in range(count,0,-1):
+#     print(i)
+#     time.sleep(1)
+# print("liftoff")
+# import random
+random.random()
