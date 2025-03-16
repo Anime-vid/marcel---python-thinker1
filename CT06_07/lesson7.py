00d6ab8dd545dcccf2a18bc1b387173e2acaf5f4
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 03:17:12 +0000

    Mar 16, 2025, 3:17 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -22,6 +22,6 @@
     
 # print(str(i) * i)
 for i in range(1,6):
-    sum = + int(input("what is student " + str(i) + " score?"))
+    sum += int(input("what is student " + str(i) + " score?"))
 sum = sum / 5
 print(sum)
