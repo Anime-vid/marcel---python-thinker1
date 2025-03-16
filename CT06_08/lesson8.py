53febceaabbb8e5d1f070e247896cd5163f2bc50
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 04:04:14 +0000

    Mar 16, 2025, 4:04 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,7 +4,7 @@
 #     print(i)
 #     time.sleep(1)
 # print("liftoff")
-import random
-print(random.randint(1,6))
-for i in range(20):
-    print(random.randint(0,9999))
+# import random
+# print(random.randint(1,6))
+# for i in range(20):
+#     print(random.randint(0,9999))
