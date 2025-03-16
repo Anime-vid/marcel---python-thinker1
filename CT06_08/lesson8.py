eb75a6a53bb8e20b27dfa06db83e3b987037be64
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 04:08:19 +0000

    Mar 16, 2025, 4:08 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,13 +4,16 @@
 #     print(i)
 #     time.sleep(1)
 # print("liftoff")
-# import random
+import random
 # print(random.randint(1,6))
 # for i in range(20):
 #     print(random.randint(0,9999))
-var1 = True
-var2 = False
-print(var1 == var2)
-var1 = True
-var2 = True
-print(var1 == var2)
+# var1 = True
+# var2 = False
+# print(var1 == var2)
+# var1 = True
+# var2 = True
+# print(var1 == var2)
+num1 = (random.randint(1,50))
+num2 = (random.randint(1,50))
+ans = input 
