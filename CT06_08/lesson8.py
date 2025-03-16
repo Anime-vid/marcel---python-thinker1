c66531d6704f2e07ee666033ae355f9da8763f89
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-03-16 04:09:54 +0000

    Mar 16, 2025, 4:09 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -16,5 +16,5 @@ import random
 # print(var1 == var2)
 num1 = (random.randint(1,50))
 num2 = (random.randint(1,50))
-ans = input("what is num1 + num2")
+ans = input("what is " + num1 + "+" + num2)
 print(num1 + num2 == int(ans))
