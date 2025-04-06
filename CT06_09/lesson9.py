e56dab41f586a663ca71ccf2c7ee4bda161b4347
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-04-06 02:54:53 +0000

    Apr 6, 2025, 2:54 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,22 @@
-print("Hello from lesson 9")
+# pos_days = 0
+# num_days = int(input("how many days?"))
+# temp_days = 0
+# for i in range(1,num_days + 1):
+#     temp_days = int(input("temp of day " + str(i) + ":"))
+#     if temp_days > 30:
+#         pos_days = pos_days + 1
+# print(pos_days)
+good = 0
+num_cus = int(input("how many customers?"))
+bad = 0
+rating = 0
+for i in range(1,num_cus + 1):
+    rating = int(input("rating of customer? " + str(i) + ":"))
+    if rating > 3:
+        good = good + 1
+    else:
+        bad = bad + 1  
+print("amount of good reviews")
+print(good)
+print("amount of bad reviews")
+print(bad)
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,25 @@
-print("Hello from lesson 10")
+# age = int(input("age?"))
+#  if num > 0:
+#      print("pos")
+#  else:
+#      print("neg")
+#  if num % 2 == 0:
+#      print("even")
+#  else:
+#     print("odd")
+# if age > 13:
+#     print("child")
+# if age < 13:
+#     print("teen")
+# if age < 18:
+#     print("adult")
+if grade > 89:
+    print("A")
+elif grade > 79:
+    print("B")
+elif grade > 69:
+    print("C")
+elif grade > 59:
+    print("D")
+elif grade > 0:
+    print("YOU ARE A FAILURE")
