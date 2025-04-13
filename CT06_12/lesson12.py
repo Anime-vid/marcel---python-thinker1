4054cfef77d0a4301f144da9e810641a11716873
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-04-13 04:12:29 +0000

    Apr 13, 2025, 4:12 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -15,5 +15,11 @@
 order = "wswnj"
 while True:
     item = input("what u want?")
-    order  += "," + item
+    order += item
     if item == "end":
+        print(order)
+        break
+    elif order == "end":
+        break
+    else:
+        order += "," + item
