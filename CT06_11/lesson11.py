97c5fb769da7806d7a5c079b924661297e6e371f
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-04-13 03:02:50 +0000

    Apr 13, 2025, 3:02 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,33 @@
-print("Hello from lesson 11")
+# px = int(input("what is the price?: "))
+# if px <= 5: print("Sounds good!")
+# elif px <= 50: print("Are you sure you need this?")
+# elif px <= 500: print("Where are you getting this money from?!")
+# else: print("Don't even think about 
+# rider1 = 18
+# rider2 = 6
+# result = rider1 >= 18 or rider2 >= 18 # >=
+# print(result)
+# num = int(input("pls num? :"))
+# if num % 7 == 0 and num % 3 == 0:
+#     print("The number is divisible by 3 and 7!")
+# age = int(input("age?"))
+# if age > 65 or age < 12:
+#     print("$15")
+# else:
+#     print("$20")
+realpass = "sjbauhwbh"
+realuser = "jsnnasi"
+User = input("user?")
+password = input("pass?")
+if  password == realpass and User == realuser: 
+    print("allowed")
+elif password == realpass or User == realuser: 
+    print("Either username or password is incorrect")
+else:
+    print()
+
+# burg = input("burg")
+# dwink = input("dwink?")
+# frys = input("frys?")
+# if burg == "yes" and frys == "yes" and not dwink == "yes":
+#     print("add dwrink now!!!!")
