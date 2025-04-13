c804c6fec469643ce6285028775d121f786580c3
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-04-13 03:19:26 +0000

    Apr 13, 2025, 3:19 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,4 +1,4 @@
-if num % 3 == 0 and num % 5 == 0:
-    print("The number is divisible by 3 and 5!")
-else:
-    print("The number is not divisible by 3 and 5!")
+# if num % 3 == 0 and num % 5 == 0:
+#     print("The number is divisible by 3 and 5!")
+# else:
+#     print("The number is not divisible by 3 and 5!")
