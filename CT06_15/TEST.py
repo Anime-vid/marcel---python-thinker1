d6ef29908a2018ee1ba83f24d3556e7288ac7afb
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-05-04 04:08:45 +0000

    May 4, 2025, 4:08 AM

--- CT06_15/TEST.py
+++ CT06_15/TEST.py
@@ -1,19 +1,3 @@
-# num = 10
-# while num != 210:
-#     print(num)
-#     num += 10
-# 1. Write code below to print the 3rd item 
-#    in this list using index e.g. earth
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
-print(planets[2])
-# 2. Write code to append neptune to this list.
-planets.append("neptune")
-#    Rename Mars in the list to be "muskworld"
-planets[3] = "muskworld"
-planets.pop(6)
-# 4. Remove uranus from this list.
-# 5. Using a for loop, print all the planets  from this list one by one.
-counter = 0
-for i in range(7):
-    print(planets[i])
-    counter = counter + 1
+for i in range(99):
+    print(i)
+    break
