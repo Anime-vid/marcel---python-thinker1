bfa2de2d290b7c54e14856ebe70692ec49cef669
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-05-04 03:42:43 +0000

    May 4, 2025, 3:42 AM

--- CT06_15/TEST.py
+++ CT06_15/TEST.py
@@ -5,5 +5,5 @@
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 print(planets[3])
 planets.append("neptune")
-planets[4] = ""
+planets[4] = "muskworld"
 print(planets)
