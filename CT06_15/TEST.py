075997e10c78aa1cedf28bfb75205d817d38015b
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-05-04 03:50:16 +0000

    May 4, 2025, 3:50 AM

--- CT06_15/TEST.py
+++ CT06_15/TEST.py
@@ -6,7 +6,7 @@
 #    in this list using index e.g. earth
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 print(planets[3])
-
+# 2. Write code to append neptune to this list.
 planets.append("neptune")
 planets[3] = "muskworld"
 planets.pop(6)
