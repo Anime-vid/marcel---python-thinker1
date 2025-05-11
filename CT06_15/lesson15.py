2867351a88010077ebc4110540b1fd02a3f76288
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2025-05-11 03:48:10 +0000

    May 11, 2025, 3:48 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,16 +1,18 @@
-def funsd():
-    import turtle
-    window = turtle.Screen()
-    window.setup(width=600, height=400)
-    t = turtle.Turtle()
-    t.shape("turtle")
-    t.fillcolor("orange")
-# t.shape("square")
-# t.fillcolor("green")
-    t.pendown()
+# def funsd():
+#     import turtle
+#     window = turtle.Screen()
+#     window.setup(width=600, height=400)
+#     t = turtle.Turtle()
+#     t.shape("turtle")
+#     t.fillcolor("orange")
+# # t.shape("square")
+# # t.fillcolor("green")
+#     t.pendown()
 
-    for i in range(360):
-        t.left(100)
-        t.forward(100)
-    window.mainloop()
-funsd()
+#     for i in range(360):
+#         t.left(100)
+#         t.forward(100)
+#     window.mainloop()
+# funsd()
+def cou():
+    
